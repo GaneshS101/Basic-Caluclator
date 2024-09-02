29694db4990c43f55fcc39fb71eaced66d98f9b1
GangeshS101 <ganeshsagi16@gmail.com>
2024-09-02 18:38:34 -0400

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+Num1 = int(input())
+symbol = str(input())
+Num2 = int(input())
+if symbol == "+":
+    Output = Num1 + Num2
+if symbol == "-":
+    Output = Num1 - Num2
+if symbol == "x":
+    Output = Num1 * Num2
+if symbol == "/":
+    Output = Num1/Num2
+print(Output)
